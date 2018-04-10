5cd43d6767dd89a46f5931956cf4e31a2e9a5df7
Thomas <thomasg70360@gmail.com>
2018-04-10 15:09:36 +0200

    Canvas initialised

--- js/game.js
+++ js/game.js
@@ -1,9 +1,30 @@
 window.onload = init;
 
+let pixelPos = true;
+
+let canvas;
+
 function init() {
+    canvas = document.querySelector("#myCanvas");
+
+    if (!pixelPos) {
+    resizeCanvas();
+
+    window.addEventListener('resize', resizeCanvas, false);
+    } else {
+        canvas.width = "500";
+        canvas.height = "500";
+    }
     //Ecouteurs de clavier
     window.onkeydown = keyDown;
     window.onkeyup = keyUp;
 
 
+}
+
+function resizeCanvas() {
+    let canvas = document.querySelector('#myCanvas');
+
+    canvas.width = window.innerWidth * 0.95;
+    canvas.height = window.innerHeight * 0.95;
 }
