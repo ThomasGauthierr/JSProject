90ce900444e8bbed2920874fc6f4941fb0a9780f
Thomas <thomasg70360@gmail.com>
2018-04-11 22:15:00 +0200

    Removing useless function

--- js/timer.js
+++ js/timer.js
@@ -4,29 +4,6 @@ let end = 0
 let currentTime = 0
 let timerID = 0
 
-function chrono2(){
-	end = new Date()
-	diff = end - start
-	diff = new Date(diff)
-	let msec = diff.getMilliseconds()
-	let sec = diff.getSeconds()
-	let min = diff.getMinutes()
-	let hr = diff.getHours()-1
-	if (min < 10){
-		min = "0" + min
-	}
-	if (sec < 10){
-		sec = "0" + sec
-	}
-	if(msec < 10){
-		msec = "00" +msec
-	}
-	else if(msec < 100){
-		msec = "0" +msec
-	}
-	timerID = setTimeout("chrono()", 10)
-}
-
 function chrono() {
     let diff = new Date() - start;
     diff = new Date(diff);
