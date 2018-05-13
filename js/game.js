37b30e9cbdb82dad872648441c9b3285934d4a01
Thomas <thomasg70360@gmail.com>
2018-05-13 02:13:35 +0200

    Highscore fixed

--- js/game.js
+++ js/game.js
@@ -200,50 +200,24 @@ function loseGame(remainingTime) {
 function endGame() {
     stopSound(inGameMusic);
     chronoStop();
-    let message = "End of the game\n";
-    message += "Score player 1 : " + players[0].score;
-
-    let highscorePlayer;
-
-    if (numberOfPlayers == 2) {
-        message += "\nScore player 2 : " + players[1].score;
-    }
-
-    if (numberOfPlayers == 1) {
-        if (players[0].score > highscore1P) {
-            highscore1P = players[0].score;
-            document.cookie = "highscore1P=" + highscore1P + ";expires=Tue, 01 Jan 2019 00:00:01 GMT";
-            message += "\nYou just set highscore !";
-            highscorePlayer = 1;
-        }
-    } else {
-        if (players[0].score > highscore2P &&
-            players[0].score > players[1].score) {
-            highscore2P = players[0].score;
-            document.cookie = "highscore2P=" + highscore2P + ";expires=Tue, 01 Jan 2019 00:00:01 GMT";
-            message += "\nPlayer 1 just set a new highscore !";
-            highscorePlayer = 1;
-        }
-
-        if (players[1].score > highscore2P &&
-            players[1].score > players[0].score) {
-            highscore2P = players[1].score;
-            document.cookie = "highscore2P=" + highscore2P + ";expires=Tue, 01 Jan 2019 00:00:01 GMT";
-            message += "\nPlayer 2 just set a new highscore !";
-            highscorePlayer = 2;
-        }
-
-    }
 
     //alert(message)
 
     canvasTimer.style.visibility = "hidden";
     //state = STATE_MAIN_MENU;
-    currentLevel = 1;
 
     state = STATE_TRANSITION_OVER;
-    drawTransition(highscorePlayer);
+    if (currentLevel > maxLevel) {
+        drawTransition(checkHighScore(currentLevel > maxLevel));
+    } else {
+        if (numberOfPlayers == 1) {
+            drawTransition(checkHighScore(players[0].lives == 0));
+        } else {
+            drawTransition(checkHighScore(players[0].lives == 0 && players[1].lives == 0));
+        }
+    }
 
+    currentLevel = 1;
 
     //requestAnimationFrame(mainMenuAnimation);
 }
@@ -255,11 +229,12 @@ function winGame() {
     if (currentLevel > maxLevel) {
         stopSound(inGameMusic);
         state = STATE_TRANSITION_OVER;
+        drawTransition(checkHighScore(currentLevel > maxLevel));
     } else {        
         state = STATE_TRANSITION_WIN;
+        drawTransition();
     }
 
-    drawTransition();
 }
 
 function resetLevel() {
@@ -306,4 +281,36 @@ function nextLevel() {
     currentLevel++;
 
     resetLevel();
+}
+
+function checkHighScore(final) {
+    let highscorePlayer;
+
+    //ToDo : final sert juste à savoir si on a fini le jeu, à voir si c'est vraiment utile (si on le met pas le highscore sera actualisé à la fin de chaque niveau ou à chaque mort)
+    if (final == true) {
+
+        if (numberOfPlayers == 1) {
+            if (players[0].score > highscore1P) {
+                highscore1P = players[0].score;
+                document.cookie = "highscore1P=" + highscore1P + ";expires=Tue, 01 Jan 2019 00:00:01 GMT";
+                highscorePlayer = 1;
+            }
+        } else {
+            if (players[0].score > highscore2P &&
+                players[0].score > players[1].score) {
+                highscore2P = players[0].score;
+                document.cookie = "highscore2P=" + highscore2P + ";expires=Tue, 01 Jan 2019 00:00:01 GMT";
+                highscorePlayer = 1;
+            }
+
+            if (players[1].score > highscore2P &&
+                players[1].score > players[0].score) {
+                highscore2P = players[1].score;
+                document.cookie = "highscore2P=" + highscore2P + ";expires=Tue, 01 Jan 2019 00:00:01 GMT";
+                highscorePlayer = 2;
+            }
+        }
+
+    }
+    return highscorePlayer;
 }
