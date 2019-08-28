6401d8fa973d673854f224484504fb96857cf988
Taoufik Fekih <tafekih@outlook.fr>
2019-08-28 14:36:49 +0200

    Server for the game

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+app.use(express.static(__dirname))
+
+
+app.get('/',(req,res)=>{
+
+res.sendFile(__dirname + '/index.html');
+})
+
+app.listen(3000,()=>{
+console.log("Server is up an running on http://localhost:3000")
+})
